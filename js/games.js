3e042fe259a9de40bd7a1dea1eb56d0cd8bd7f7a
Nouredine227 <abdoumayakinouredine@gmail.com>
2024-12-03 18:00:53 +0100

    Update games.js

--- js/games.js
+++ js/games.js
@@ -1,15 +1,16 @@
-const commentForm = document.getElementById('comment-form');
-const commentList = document.getElementById('comment-list');
-
-commentForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    
-    const username = document.getElementById('username').value;
-    const comment = document.getElementById('comment').value;
-    
-    const newComment = document.createElement('li');
-    newComment.innerHTML = `<strong>${username}:</strong> ${comment}`;
-    commentList.appendChild(newComment);
-    
-    commentForm.reset();
-});
+fetch('data/games.json')
+    .then(response => response.json())
+    .then(data => {
+        const gameList = document.getElementById('game-list');
+        data.games.forEach(game => {
+            const gameItem = document.createElement('div');
+            gameItem.classList.add('game');
+            gameItem.innerHTML = `
+                <img src="assets/images/${game.icon}" alt="${game.name}">
+                <h2>${game.name}</h2>
+                <p>${game.description}</p>
+                <a href="${game.downloadLink}" class="button" download>Télécharger</a>
+            `;
+            gameList.appendChild(gameItem);
+        });
+    });
