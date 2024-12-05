c7f61072ac4b1ab6c932b220322df1ac2afdd149
Nouredine227 <abdoumayakinouredine@gmail.com>
2024-12-05 22:57:17 +0100

    Update games.js

--- js/games.js
+++ js/games.js
@@ -39,21 +39,3 @@ fetch("data/games.json")
         displayGames(games);
     })
     .catch(error => console.error("Erreur de chargement des jeux :", error));
-
-// Gestion du mode sombre/clair
-const toggleThemeButton = document.querySelector(".toggle-theme");
-toggleThemeButton.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
-    localStorage.setItem(
-        "theme",
-        document.body.classList.contains("dark-mode") ? "dark" : "light"
-    );
-});
-
-// Charger le thème choisi
-document.addEventListener("DOMContentLoaded", () => {
-    const savedTheme = localStorage.getItem("theme");
-    if (savedTheme === "dark") {
-        document.body.classList.add("dark-mode");
-    }
-});
