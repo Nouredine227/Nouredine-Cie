2113fcf666b0cb2d337e82bbdb8d765350653e6f
Nouredine227 <abdoumayakinouredine@gmail.com>
2024-12-09 21:24:28 +0100

    Create about.css

---
+++ css/about.css
@@ -0,0 +1,31 @@
+/* Styles pour les sections des organisations */
+.organization-info {
+  margin-bottom: 30px;
+  padding: 15px;
+  background: #222;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  color: #f0f0f0;
+}
+
+.organization-logo {
+  display: block;
+  margin: 20px auto;
+  max-width: 150px;
+  border-radius: 50%;
+  border: 2px solid #fff;
+}
+
+.donation-link {
+  display: inline-block;
+  margin-top: 15px;
+  padding: 10px 15px;
+  background-color: #d9534f;
+  color: #fff;
+  text-decoration: none;
+  border-radius: 5px;
+}
+
+.donation-link:hover {
+  background-color: #c9302c;
+}
