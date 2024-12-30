539f31fa9f57677bcdc84d682c25fe730516a3e5
Nouredine227 <abdoumayakinouredine@gmail.com>
2024-12-30 17:48:18 +0100

    Delete js/updates.js

--- js/updates.js
+++
@@ -1,8 +0,0 @@
-fetch('data/updates.json')
-    .then(response => response.json())
-    .then(data => {
-        const updateLog = document.getElementById('updateLog');
-        updateLog.innerHTML = data.updates.map(update => `
-            <li>${update.date}: ${update.description}</li>
-        `).join('');
-    });
